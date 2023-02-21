8017745c9e46330f11f3c774b6c14f24d46fab7b
RaviHotkar <85622404+RaviHotkar@users.noreply.github.com>
2023-02-21 15:52:24 +0530

    Add files via upload

---
+++ Mega_Eeprom_Custom/Mega_Eeprom_Custom.h
@@ -0,0 +1,13 @@
+#ifndef Mega_Eeprom_Custom_h
+#define Mega_Eeprom_Custom_h
+
+class Mega_Eeprom_Custom
+{
+	public 	  :			
+				void EEPROM_read();
+	private   :			
+				
+	protected : 
+};
+
+#endif
